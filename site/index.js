3da742e180c55abc106e2fceca65f87e6f5c7804
Tdubz06 <105948776+Tdubz06@users.noreply.github.com>
2023-09-26 18:04:39 -0400

    Update index.js

--- site/index.js
+++ site/index.js
@@ -18,95 +18,89 @@ fetch(url)
     });
 
     const leaderboardDiv = document.querySelector('.leaderboard');
-
     sortedData.forEach((user, index) => {
       const username = user.username;
-      //seperate the amounts number with commas
-      // const amount = parseFloat(user.amount);
-      sortedData.forEach((user, index) => {
-        const username = user.username;
-        const amount = parseFloat(user.amount).toLocaleString(undefined, {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2
-        });
-
-
-
-        // Create a new div element for each user
-        const personDiv = document.createElement('div');
-        personDiv.classList.add('person');
-        personDiv.setAttribute('rank', index + 1);
-
-        // Create the left div
-        const leftDiv = document.createElement('div');
-        leftDiv.classList.add('left');
-
-        // Create the person name div
-        const personNameDiv = document.createElement('div');
-        personNameDiv.classList.add('person-name');
-        personNameDiv.textContent = username;
-
-        // Create the person balance div
-        const personBalanceDiv = document.createElement('div');
-        personBalanceDiv.classList.add('person-ballance');
-        personBalanceDiv.textContent = amount;
-        const tokenShortSpan = document.createElement('span');
-        tokenShortSpan.classList.add('token-short');
-        tokenShortSpan.textContent = '\u00A0TDGC'; // Using '\u00A0' for non-breaking space
-
-        personBalanceDiv.appendChild(tokenShortSpan);
-
-        // Append name and balance to left div
-        leftDiv.appendChild(personNameDiv);
-        leftDiv.appendChild(personBalanceDiv);
-
-        // Create the right div for rank
-        const rightDiv = document.createElement('div');
-        rightDiv.classList.add('right');
-
-        // Create the person rank div
-        const personRankDiv = document.createElement('div');
-        personRankDiv.classList.add('person-rank');
-
-        // Check if it's one of the top three ranks
-        if (index < 3) {
-          // Create an img element for the medal
-          if (index == 0) {
-            const medalImg = document.createElement('img');
-            medalImg.setAttribute('src', `${index + 1}st-prize-2.png`);
-            medalImg.setAttribute('alt', `${index + 1}-place-medal`);
-            personRankDiv.appendChild(medalImg);
-          }
-          else if (index == 1) {
-            const medalImg = document.createElement('img');
-            medalImg.setAttribute('src', `${index + 1}nd-place-2.png`);
-            medalImg.setAttribute('alt', `${index + 1}-place-medal`);
-            personRankDiv.appendChild(medalImg);
-          }
-          else if (index == 2) {
-            const medalImg = document.createElement('img');
-            medalImg.setAttribute('src', `${index + 1}rd-place-2.png`);
-            medalImg.setAttribute('alt', `${index + 1}-place-medal`);
-            personRankDiv.appendChild(medalImg);
-          }
-        } else {
-          // Otherwise, use the regular rank number
-          personRankDiv.textContent = index + 1;
-        }
-
-        // Append rank to right div
-        rightDiv.appendChild(personRankDiv);
-
-        // Append left and right divs to person div
-        personDiv.appendChild(leftDiv);
-        personDiv.appendChild(rightDiv);
-
-        // Append the person div to the leaderboard div
-        leaderboardDiv.appendChild(personDiv);
+      const amount = parseFloat(user.amount).toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
       });
-    });
 
+
+
+      // Create a new div element for each user
+      const personDiv = document.createElement('div');
+      personDiv.classList.add('person');
+      personDiv.setAttribute('rank', index + 1);
+
+      // Create the left div
+      const leftDiv = document.createElement('div');
+      leftDiv.classList.add('left');
+
+      // Create the person name div
+      const personNameDiv = document.createElement('div');
+      personNameDiv.classList.add('person-name');
+      personNameDiv.textContent = username;
+
+      // Create the person balance div
+      const personBalanceDiv = document.createElement('div');
+      personBalanceDiv.classList.add('person-ballance');
+      personBalanceDiv.textContent = amount;
+      const tokenShortSpan = document.createElement('span');
+      tokenShortSpan.classList.add('token-short');
+      tokenShortSpan.textContent = '\u00A0TDGC'; // Using '\u00A0' for non-breaking space
+
+      personBalanceDiv.appendChild(tokenShortSpan);
+
+      // Append name and balance to left div
+      leftDiv.appendChild(personNameDiv);
+      leftDiv.appendChild(personBalanceDiv);
+
+      // Create the right div for rank
+      const rightDiv = document.createElement('div');
+      rightDiv.classList.add('right');
+
+      // Create the person rank div
+      const personRankDiv = document.createElement('div');
+      personRankDiv.classList.add('person-rank');
+
+      // Check if it's one of the top three ranks
+      if (index < 3) {
+        // Create an img element for the medal
+        if (index == 0) {
+          const medalImg = document.createElement('img');
+          medalImg.setAttribute('src', `${index + 1}st-prize-2.png`);
+          medalImg.setAttribute('alt', `${index + 1}-place-medal`);
+          personRankDiv.appendChild(medalImg);
+        }
+        else if (index == 1) {
+          const medalImg = document.createElement('img');
+          medalImg.setAttribute('src', `${index + 1}nd-place-2.png`);
+          medalImg.setAttribute('alt', `${index + 1}-place-medal`);
+          personRankDiv.appendChild(medalImg);
+        }
+        else if (index == 2) {
+          const medalImg = document.createElement('img');
+          medalImg.setAttribute('src', `${index + 1}rd-place-2.png`);
+          medalImg.setAttribute('alt', `${index + 1}-place-medal`);
+          personRankDiv.appendChild(medalImg);
+        }
+      } else {
+        // Otherwise, use the regular rank number
+        personRankDiv.textContent = index + 1;
+      }
+
+      // Append rank to right div
+      rightDiv.appendChild(personRankDiv);
+
+      // Append left and right divs to person div
+      personDiv.appendChild(leftDiv);
+      personDiv.appendChild(rightDiv);
+
+      // Append the person div to the leaderboard div
+      leaderboardDiv.appendChild(personDiv);
+    });
   })
-  .catch(error => {
-    console.error("Fetch error:", error);
-  });
+  
+  .catch (error => {
+  console.error("Fetch error:", error);
+});
